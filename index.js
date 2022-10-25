9a9649812e9675bf20e9f728d5b75b5aa7fe6821
Joseph McGloin <s00212257@atu.ie>
2022-10-25 15:02:04 +0100

    Add variable for key

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 //TODO: Set up process.env.PORT etc.
 const port = 3000;
 
+//TODO: Create an API key in .env
+const key = "";
+
 app.use(express.json());
 
 app.get('/', (res) => res.send('API Online'));
