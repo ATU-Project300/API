6b8b2d679b81a4dff053de566b4607335c28d19f
s00212257 <s00212257@atu.ie>
2022-11-22 05:53:16 +0000

    Add game model and verification, add game route

--- models/games.js
+++ models/games.js
@@ -2,9 +2,25 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const gameSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    year: Number,
+    description: String,
+    image: String,
+    consoles: String,
+    emulator: String,
 
 })
 
 function ValidateGame(game){
+    const gameJoiSchema = Joi.object(
+        {
+            title: Joi.required(),
+            year: Joi.Number.integer.min(1950),
+            description: Joi.String().min(10),
+            image: Joi.String().min(8),
+            consoles: Joi.String().min(2),
+            emulator: Joi.String()
+        }
+    )
     
 }
--- routes/games.js
+++ routes/games.js
@@ -3,3 +3,56 @@ const Joi = require('joi');
 const router = express.Router();
 
 const { Game } = require('../models/games')
+
+
+function ValidateGame(game) {
+    const gameJoiSchema = Joi.object(
+        {
+            title: Joi.required(),
+            year: Joi.Number.integer.min(1950),
+            description: Joi.String().min(10),
+            image: Joi.String().min(8),
+            consoles: Joi.String().min(2),
+            emulator: Joi.String()
+        }
+    )
+}
+
+//TODO: Test POST
+router.post('/', async (req, res) => {
+    let result = ValidateGame(req.body)
+
+    if (result.error) {
+        res.status(400).json(result.error);
+        return;
+    }
+
+    let game = new Game(req.body);
+    try {
+        game = await game.save();
+        res
+            .location(`${game._id}`)
+            .status(201)
+            .json(game)
+    }
+    catch (error) {
+        res.status(500).send('db_error ' + error)
+    }
+
+});
+
+//TODO: Add filters
+router.get('/', async (req, res) => {
+    const { title } = req.query;
+    let filter = {};
+
+    title ? filter.title = title : null;
+
+    try {
+        const games = await Game.find(title);
+        res.json(games);
+    }
+    catch{
+        res.status(404).json('Not found');
+    }
+})
