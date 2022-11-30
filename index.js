aab458b6a9819c2392082d5c15b0b9cecf6c1daf
s00212257 <s00212257@atu.ie>
2022-11-30 15:15:48 +0000

    Fix CORS

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ const limiter = rateLimit({
 
 app.use(limiter)
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 app.use(express.json());
 
 app.use('/games', games)
