52a7ea541ccc596b2ec1188c58e5f7e17ee600e6
s00212257 <s00212257@atu.ie>
2022-12-01 11:02:59 +0000

    Fix emulator endpoint issues

--- routes/emulators.js
+++ routes/emulators.js
@@ -2,7 +2,6 @@ const express = require('express');
 const Joi = require('joi');
 const router = express.Router();
 const { config } = require('dotenv').config();
-
 const { Emulator } = require('../models/emulator')
 
 //TODO: GET by ID, DELETE, PUT
@@ -11,9 +10,9 @@ function ValidateEmulator(emulator) {
     const emulatorJoiSchema = Joi.object(
         {
             title: Joi.required(),
-            description: Joi.String().min(10),
-            image: Joi.String().min(8),
-            console: Joi.String().min(2),
+            description: Joi.string().min(10),
+            image: Joi.string().min(8),
+            console: Joi.string().min(2),
         }
     )
   return emulatorJoiSchema.validate(emulator);
@@ -70,5 +69,18 @@ router.get('/', async (req, res) => {
         res.status(404).json('Not found');
     }
 });
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const emulator = await Emulator.findByIdAndDelete(req.params.id);
+        if (emulator)
+            res.status(204).send();
+        else
+            res.status(404).json(`Emulator with ID ${req.params.id} not found`)
+    }
+    catch {
+        res.status(404).json(`Error caused by ID: ${req.params.id}`);
+    }
+})
 
 module.exports = router;
